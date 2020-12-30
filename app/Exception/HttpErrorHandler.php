9e05827653db1748838959bb1997d2683385bb99
where.liu <where.liu@huolala.cn>
2020-12-30 09:25:46 +0800

    feat(Exception): 响应 ValidationException

--- app/Exception/HttpErrorHandler.php
+++ app/Exception/HttpErrorHandler.php
@@ -1,21 +1,28 @@
 <?php
 
-
 namespace App\Exception;
 
-
+use Illuminate\Validation\ValidationException;
 use Psr\Http\Message\ResponseInterface;
 use Slim\Exception\HttpMethodNotAllowedException;
 use Slim\Handlers\ErrorHandler;
+use Slim\Http\Response;
 
 class HttpErrorHandler extends ErrorHandler
 {
     protected function respond(): ResponseInterface
     {
+        /**
+         * @var Response
+         */
         $response = $this->responseFactory->createResponse();
-        if ($this->exception instanceof HttpMethodNotAllowedException){
+        if ($this->exception instanceof HttpMethodNotAllowedException) {
             return $response->withStatus(405);
         }
+        if ($this->exception instanceof ValidationException) {
+            return $response->withJson($this->exception->errors(), 422);
+        }
+
         return parent::respond();
     }
 }
