781c612de605db10af624841af3454ba8f287fcd
where.liu <where.liu@huolala.cn>
2021-01-04 13:33:45 +0800

    feat: db

--- app/Action/HealthAction.php
+++ app/Action/HealthAction.php
@@ -2,14 +2,29 @@
 
 namespace App\Action;
 
+use App\Model\UserModel;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Psr\Http\Message\ResponseInterface;
 
 class HealthAction extends Action
 {
     protected function action(): ResponseInterface
     {
-        $data = [];
+        $data = [
+            'database' => $this->checkDatabase(),
+        ];
 
         return $this->response->withJson($data);
+    }
+
+    private function checkDatabase(): bool
+    {
+        try {
+            UserModel::query()->firstOrFail();
+
+            return true;
+        } catch (ModelNotFoundException $exception) {
+            return false;
+        }
     }
 }
