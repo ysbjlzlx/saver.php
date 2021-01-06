f81807733a7e0cb1b6fc8413dbd56f84bac15a9d
where.liu <where.liu@huolala.cn>
2021-01-06 09:50:45 +0800

    feat: store

--- app/Service/UserTokenService.php
+++ app/Service/UserTokenService.php
@@ -2,8 +2,10 @@
 
 namespace App\Service;
 
+use App\Model\UserModel;
 use App\Model\UserTokenModel;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Str;
 
 class UserTokenService
 {
@@ -22,5 +24,37 @@ class UserTokenService
         } catch (ModelNotFoundException $exception) {
             return null;
         }
+    }
+
+    /**
+     * @param UserModel $userModel 用户
+     * @param array{$ua: string, $ip: string} $params    登录参数
+     *
+     * @return UserTokenModel 登录记录
+     */
+    public function store(UserModel $userModel, array $params): UserTokenModel
+    {
+        $userTokenModel = new UserTokenModel();
+        $userTokenModel->user_id = $userTokenModel->id;
+        $userTokenModel->token = $this->generateToken($userModel);
+        $userTokenModel->ua = $params['ua'] ?? '';
+        $userTokenModel->ip = $params['ip'] ?? '';
+        $userTokenModel->save();
+
+        return $userTokenModel;
+    }
+
+    /**
+     * @param UserModel|null $userModel 用户
+     *
+     * @return string token
+     */
+    private function generateToken(UserModel $userModel = null): string
+    {
+        $id = is_null($userModel) ? '' : $userModel->id;
+        $current = microtime(true);
+        $randomStr = Str::random(64);
+
+        return md5($current.$randomStr.$id);
     }
 }
