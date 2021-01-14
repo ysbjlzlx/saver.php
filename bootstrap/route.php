e9defec10a14fd7d2e90ad3475f4b3dd4374b834
where.liu <where.liu@huolala.cn>
2021-01-14 12:40:47 +0800

    feat(Cache): 更新缓存接口

--- bootstrap/route.php
+++ bootstrap/route.php
@@ -1,44 +1,43 @@
 <?php
 
-use App\Action\Account\DestroyAccountAction;
-use App\Action\Auth\LoginAction;
-use App\Action\Auth\LogoutAction;
-use App\Action\Auth\RegisterAction;
-use App\Action\HealthAction;
-use App\Action\SwaggerUiAction;
-use App\Middleware\AuthMiddleware;
-use Slim\Routing\RouteCollectorProxy;
+declare(strict_types=1);
 
 return function (Slim\App $app) {
-    $app->any('/health', HealthAction::class);
-    $app->get('/swagger-ui', SwaggerUiAction::class);
+    $app->any('/health', App\Action\HealthAction::class);
+    $app->get('/swagger-ui', App\Action\SwaggerUiAction::class);
 
     /*
      * account
      */
-    $app->group('', function (RouteCollectorProxy $group) {
-        $group->post('/api/account/delete', DestroyAccountAction::class);
-    })->add(AuthMiddleware::class);
+    $app->group('', function (Slim\Routing\RouteCollectorProxy $group) {
+        $group->post('/api/account/delete', App\Action\Account\DestroyAccountAction::class);
+    })->add(App\Middleware\AuthMiddleware::class);
+
     /*
      * auth
      */
-    $app->post('/api/auth/register', RegisterAction::class);
-    $app->post('/api/auth/login', LoginAction::class);
-    $app->post('/api/auth/logout', LogoutAction::class)->add(AuthMiddleware::class);
+    $app->post('/api/auth/register', App\Action\Auth\RegisterAction::class);
+    $app->post('/api/auth/login', App\Action\Auth\LoginAction::class);
+    $app->post('/api/auth/logout', App\Action\Auth\LogoutAction::class)
+        ->add(App\Middleware\AuthMiddleware::class);
+
     /*
      * home
      */
-    $app->get('/api/home', App\Action\Home\HomeAction::class)->add(AuthMiddleware::class);
+    $app->get('/api/home', App\Action\Home\HomeAction::class)
+        ->add(App\Middleware\AuthMiddleware::class);
+
     /*
      * upload
      */
-    $app->post('/api/upload/store', \App\Action\Upload\StoreAction::class);
-    $app->get('/api/upload/show', \App\Action\Upload\ShowAction::class);
+    $app->post('/api/upload/store', App\Action\Upload\StoreAction::class);
+    $app->get('/api/upload/show', App\Action\Upload\ShowAction::class);
+
     /*
      * data dict
      */
     $app->get('/api/data-dict/index', App\Action\DataDict\IndexAction::class);
-    $app->get('/api/data-dict/show', \App\Action\DataDict\ShowAction::class);
-    $app->post('/api/data-dict/store', \App\Action\DataDict\StoreAction::class);
-    $app->put('/api/data-dict/update', \App\Action\DataDict\UpdateAction::class);
+    $app->get('/api/data-dict/show', App\Action\DataDict\ShowAction::class);
+    $app->post('/api/data-dict/store', App\Action\DataDict\StoreAction::class);
+    $app->put('/api/data-dict/update', App\Action\DataDict\UpdateAction::class);
 };
