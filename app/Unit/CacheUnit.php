5262d88231a777b374cef5d6eca35676d1b81041
where.liu <where.liu@huolala.cn>
2021-01-14 10:25:43 +0800

    feat(Cache): 更新缓存接口

--- app/Unit/CacheUnit.php
+++ app/Unit/CacheUnit.php
@@ -2,9 +2,13 @@
 
 namespace App\Unit;
 
+use Closure;
 use Doctrine\Common\Cache\CacheProvider;
-use Psr\SimpleCache\CacheInterface;
+use Illuminate\Contracts\Cache\Repository as CacheInterface;
 
+/**
+ * Class CacheUnit.
+ */
 class CacheUnit implements CacheInterface
 {
     /**
@@ -61,5 +65,95 @@ class CacheUnit implements CacheInterface
     public function has($key)
     {
         return $this->instance->contains($key);
+    }
+
+    public function pull($key, $default = null)
+    {
+        $value = $this->get($key, $default);
+        $this->delete($key);
+
+        return $value;
+    }
+
+    public function put($key, $value, $ttl = null)
+    {
+        return $this->set($key, $value, $ttl);
+    }
+
+    public function add($key, $value, $ttl = null)
+    {
+        if ($this->has($key)) {
+            return false;
+        }
+
+        return $this->set($key, $value, $ttl);
+    }
+
+    public function increment($key, $value = 1)
+    {
+        $origin = $this->get($key, 0);
+        if (is_numeric($origin)) {
+            return $this->set($key, $origin + $value);
+        }
+
+        return false;
+    }
+
+    public function decrement($key, $value = 1)
+    {
+        $origin = $this->get($key, 0);
+        if (is_numeric($origin)) {
+            return $this->set($key, $origin - $value);
+        }
+
+        return false;
+    }
+
+    public function forever($key, $value)
+    {
+        return $this->set($key, $value);
+    }
+
+    public function remember($key, $ttl, Closure $callback)
+    {
+        if ($this->has($key)) {
+            return $this->get($key);
+        }
+        $value = call_user_func($callback);
+        $this->set($key, $value, $ttl);
+
+        return $value;
+    }
+
+    public function sear($key, Closure $callback)
+    {
+        if ($this->has($key)) {
+            return $this->get($key);
+        }
+        $value = call_user_func($callback);
+        $this->set($key, $value);
+
+        return $value;
+    }
+
+    public function rememberForever($key, Closure $callback)
+    {
+        if ($this->has($key)) {
+            return $this->get($key);
+        }
+        $value = call_user_func($callback);
+        $this->set($key, $value);
+
+        return $value;
+    }
+
+    public function forget($key)
+    {
+        return $this->delete($key);
+    }
+
+    public function getStore()
+    {
+        // TODO: Implement getStore() method.
     }
 }
