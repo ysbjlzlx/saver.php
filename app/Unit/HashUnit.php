5f6f971f8a697ae88ae6aa314a121412a5d00232
liuzhaowei <liuzhaowei55@gmail.com>
2021-01-16 22:42:46 +0800

    feat(Log): 记录日志到数据库

---
+++ app/Unit/HashUnit.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Unit;
+
+use Illuminate\Contracts\Hashing\Hasher;
+
+class HashUnit implements Hasher
+{
+    public function info($hashedValue)
+    {
+        return password_get_info($hashedValue);
+    }
+
+    public function make($value, array $options = [])
+    {
+        return password_hash($value, PASSWORD_BCRYPT, $options);
+    }
+
+    public function check($value, $hashedValue, array $options = [])
+    {
+        return password_verify($value, $hashedValue);
+    }
+
+    public function needsRehash($hashedValue, array $options = [])
+    {
+        return password_needs_rehash($hashedValue, PASSWORD_BCRYPT, $options);
+    }
+}
