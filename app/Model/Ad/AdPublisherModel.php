37e43ebc99e87a4a4575b89176ec4570b6a50801
where.liu <where.liu@huolala.cn>
2021-01-19 10:05:49 +0800

    feat(table): 添加模型

--- app/Model/Ad/AdPublisherModel.php
+++ app/Model/Ad/AdPublisherModel.php
@@ -2,13 +2,26 @@
 
 namespace App\Model\Ad;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class AdPublisherModel.
  * 流量主模型.
+ *
+ * @property int           $id
+ * @property string        $username
+ * @property string        $password
+ * @property string        $email
+ * @property string|Carbon $email_verified_at
+ * @property string|Carbon $updated_at
+ * @property string|Carbon $created_at
  */
 class AdPublisherModel extends Model
 {
     protected $table = 'ad_publisher';
+    protected $hidden = ['password'];
+    protected $dates = [
+        'email_verified_at', 'updated_at', 'created_at',
+    ];
 }
